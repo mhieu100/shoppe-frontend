2a1951521fbb88e17cf7c43b9407b5186dded4f2
devtino2605 <168699442+devtino2605@users.noreply.github.com>
2025-04-13 13:43:14 +0700

    save changes before pull

--- src/components/modal/modal.user.jsx
+++ src/components/modal/modal.user.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Modal } from 'antd';
 const ModalUser = (props) => {
-    const { isModalOpen, setIsModalOpen } = props;
+  const { isModalOpen, setIsModalOpen } = props;
 
-    const handleOk = () => {
-        setIsModalOpen(false);
-    };
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
-    return (
-        <>
-            <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-            </Modal>
-        </>
-    );
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
 };
 export default ModalUser;
